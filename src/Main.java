ccb01b3536ae226cfbbe37fb9f113176af2dea53
Владимир <vkuznecov1504@mail.ru>
2023-12-30 13:00:57 +0300

    Инициализировали переменную apple Инициализировали переменную oranges Инициализировали переменную watermelon Инициализировали переменную lemon Инициализировали переменную pumpkin Инициализировали переменную melon

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-
+        int apple = 100;
+        System.out.println("Значение переменной  с типом равно " + apple + " кг");
+        byte oranges = 120;
+        System.out.println("Значение переменной oranges с типом byte равно " + oranges + " кг");
+        short watermelon = 300;
+        System.out.println("Значение переменной watermelon с типом short равно " + watermelon + " кг");
+        long lemon = 1_000_000_000L;
+        System.out.println("Значение переменной lemon с типом long равно " + lemon + " кг");
+        float pumpkin = 1.025f;
+        System.out.println("Значение переменной pumpkin с типом float равно " + pumpkin + " кг");
+        double melon = 1.55555555;
+        System.out.println("Значение пременной melon с типом double равно " + melon + " кг");
     }
 }
