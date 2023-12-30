299bb9bb9bf15ce84ca5bf663d81ffe1cc3e293a
Владимир <vkuznecov1504@mail.ru>
2023-12-30 13:16:44 +0300

    Инициализировали переменные int, byte, int, short, long, float, double

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+
+        //Задача 1
+
         int apple = 100;
         System.out.println("Значение переменной  с типом равно " + apple + " кг");
         byte oranges = 120;
@@ -12,5 +15,15 @@ public class Main {
         System.out.println("Значение переменной pumpkin с типом float равно " + pumpkin + " кг");
         double melon = 1.55555555;
         System.out.println("Значение пременной melon с типом double равно " + melon + " кг");
+
+        //Задача 2
+
+        int a = -159;
+        byte b = 67;
+        int c = 569;
+        short d = 27897;
+        long e = 987_678_965_549L;
+        float f = 27.12f;
+        double v = 2.786;
     }
 }
